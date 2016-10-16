9579d6db6a4f4e583bf37b70ff386a144db8ed98
dcs2106 <v0918732669@gmail.com>
2016-10-16 22:47:53 +0800

    add thread mutex

--- pi.c
+++ pi.c
@@ -1,39 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-long long int number_in_circle=0;
-void pick();
+#include <pthread.h>
+#define thread_count 4
+
+long long int number_in_circle=0,number_of_toss;
+pthread_mutex_t mutex;
+void *pick();
 int main(int argc,char *argv[])
 {
-	long long int hit,
-		      number_of_toss,
-		      toss=0;
-	clock_t start_time,end_time;
-	start_time=clock();
 	if(argc!=2){
 		printf("You should input the number of tosses\n");
 		return 0;
 	}
+	
+	long thread;
+
 	number_of_toss=atoi(argv[1]);
 	srand(time(NULL));
-
-
-	for(toss=0;toss<number_of_toss;toss++){
-		pick();
-	}
-
-
-	end_time=clock();
+	pthread_t *thread_handles;
+	pthread_t thread1,thread2,thread3,thread4;
+	thread_handles = (pthread_t *)malloc (thread_count*sizeof(pthread_t));
+	pthread_mutex_init(&mutex,NULL);//mutex initial
+	for(thread = 0; thread < thread_count ; thread++)
+		pthread_create(&thread_handles[thread],NULL,pick,NULL);
+	
+	
+	for(thread = 0; thread < thread_count ; thread++)
+		pthread_join(thread_handles[thread],NULL);
+	
+	
+	pthread_mutex_destroy(&mutex);
+	
 	printf("The answer is %f\n",4*number_in_circle/(double)number_of_toss);
-	printf("%f s\n",(double)(end_time-start_time)/1000000);
 	return 0;
 }
-void pick()
+void *pick()
 {
 	double x,y,distance_squared;
-	x = ((rand()%100000)+1)/100000.0;
-	y = ((rand()%100000)+1)/100000.0;
-	distance_squared = x*x+y*y;
-	if(distance_squared <= 1)
-		number_in_circle++;
+	long long int toss,
+		      bound=number_of_toss/thread_count;
+	for(toss=0;toss < bound;toss++){
+		x = ((rand()%100000)+1)/100000.0;
+		y = ((rand()%100000)+1)/100000.0;
+		distance_squared = x*x+y*y;
+		if(distance_squared <= 1){
+			pthread_mutex_lock(&mutex);
+			number_in_circle++;
+			pthread_mutex_unlock(&mutex);
+		}
+	}
 }
